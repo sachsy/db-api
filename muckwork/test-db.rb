00ea6661a2a0177d58b87dd9b1771e30c0d60500
Derek Sivers <derek@sivers.org>
2015-08-04 18:12:25 +0200

    + project and task status/date tests

--- muckwork/test-db.rb
+++ muckwork/test-db.rb
@@ -5,30 +5,80 @@ class TestMuckworkDB < Minitest::Test
 	def test_project_status_update
 		res = DB.exec("SELECT status FROM projects WHERE id=5")
 		assert_equal 'created', res[0]['status']
-		res = DB.exec("UPDATE projects SET quoted_at=NOW() WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET quoted_at=NOW() WHERE id=5 RETURNING status")
 		assert_equal 'quoted', res[0]['status']
-		res = DB.exec("UPDATE projects SET approved_at=NOW() WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET approved_at=NOW() WHERE id=5 RETURNING status")
 		assert_equal 'approved', res[0]['status']
-		res = DB.exec("UPDATE projects SET started_at=NOW() WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET started_at=NOW() WHERE id=5 RETURNING status")
 		assert_equal 'started', res[0]['status']
-		res = DB.exec("UPDATE projects SET finished_at=NOW() WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET finished_at=NOW() WHERE id=5 RETURNING status")
 		assert_equal 'finished', res[0]['status']
-		res = DB.exec("UPDATE projects SET finished_at=NULL WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET finished_at=NULL WHERE id=5 RETURNING status")
 		assert_equal 'started', res[0]['status']
-		res = DB.exec("UPDATE projects SET started_at=NULL WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET started_at=NULL WHERE id=5 RETURNING status")
 		assert_equal 'approved', res[0]['status']
-		res = DB.exec("UPDATE projects SET approved_at=NULL WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET approved_at=NULL WHERE id=5 RETURNING status")
 		assert_equal 'quoted', res[0]['status']
-		res = DB.exec("UPDATE projects SET quoted_at=NULL WHERE id=5")
-		res = DB.exec("SELECT status FROM projects WHERE id=5")
+		res = DB.exec("UPDATE projects SET quoted_at=NULL WHERE id=5 RETURNING status")
 		assert_equal 'created', res[0]['status']
+	end
+
+	def test_project_dates
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE projects SET quoted_at=NULL WHERE id=1")
+		end
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE projects SET approved_at=NULL WHERE id=1")
+		end
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE projects SET started_at=NULL WHERE id=1")
+		end
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE projects SET finished_at=NOW() WHERE id=4")
+		end
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE projects SET started_at=NOW() WHERE id=5")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE projects SET created_at=NOW() WHERE id=1")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE projects SET quoted_at=NOW() WHERE id=1")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE projects SET approved_at=NOW() WHERE id=1")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE projects SET started_at=NOW() WHERE id=1")
+		end
+	end
+
+	def test_task_status_update
+		res = DB.exec("SELECT status FROM tasks WHERE id=8")
+		assert_equal 'approved', res[0]['status']  # might change
+		res = DB.exec("UPDATE tasks SET started_at=NOW() WHERE id=8 RETURNING status")
+		assert_equal 'started', res[0]['status']
+		res = DB.exec("UPDATE tasks SET finished_at=NOW() WHERE id=8 RETURNING status")
+		assert_equal 'finished', res[0]['status']
+		res = DB.exec("UPDATE tasks SET finished_at=NULL WHERE id=8 RETURNING status")
+		assert_equal 'started', res[0]['status']
+		res = DB.exec("UPDATE tasks SET started_at=NULL WHERE id=8 RETURNING status")
+		assert_equal 'created', res[0]['status']
+	end
+
+	def test_task_dates
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE tasks SET started_at=NULL WHERE id=1")
+		end
+		assert_raises PG::RaiseException do
+			DB.exec("UPDATE tasks SET finished_at=NOW() WHERE id=8")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE tasks SET created_at=NOW() WHERE id=1")
+		end
+		assert_raises PG::CheckViolation do
+			DB.exec("UPDATE tasks SET started_at=NOW() WHERE id=1")
+		end
 	end
 
 end
