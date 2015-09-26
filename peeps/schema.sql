2c454eebb49d92cafc1a83a9442810a3a42235cf
Derek Sivers <derek@sivers.org>
2015-09-26 20:21:33 +1200

    opened_emails shows who opened it

--- peeps/schema.sql
+++ peeps/schema.sql
@@ -1115,8 +1115,12 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION opened_emails(integer, OUT mime text, OUT js json) AS $$
 BEGIN
 	mime := 'application/json';
-	js := json_agg(r) FROM (SELECT * FROM peeps.emails_view WHERE id IN
-		(SELECT * FROM peeps.opened_email_ids($1)) ORDER BY id) r;
+	js := json_agg(r) FROM (SELECT e.id, subject, opened_at, p.name
+		FROM peeps.emails e
+		JOIN peeps.emailers r ON e.opened_by=r.id
+		JOIN peeps.people p ON r.person_id=p.id
+		WHERE e.id IN 
+			(SELECT * FROM peeps.opened_email_ids($1)) ORDER BY opened_at) r;
 	IF js IS NULL THEN
 		js := '[]';
 	END IF;
