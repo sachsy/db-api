c100527bddd88be539d26878a73ffe2daed56bc0
Derek Sivers <derek@sivers.org>
2015-10-30 07:02:19 +1300

    add UNIQUE constraint to now.urls

--- now/schema.sql
+++ now/schema.sql
@@ -10,8 +10,8 @@ CREATE TABLE now.urls (
 	person_id integer REFERENCES peeps.people(id) ON DELETE CASCADE,
 	created_at date not null default CURRENT_DATE,
 	updated_at date,
-	short varchar(72),
-	long varchar(100) CONSTRAINT url_format CHECK (long ~ '^https?://[0-9a-zA-Z_-]+\.[a-zA-Z0-9]+')
+	short varchar(72) UNIQUE,
+	long varchar(100) UNIQUE CONSTRAINT url_format CHECK (long ~ '^https?://[0-9a-zA-Z_-]+\.[a-zA-Z0-9]+')
 );
 
 COMMIT;
