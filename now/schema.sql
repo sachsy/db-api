57d6d7f03af4a2b6ddac9008b736d5fc82199adb
Derek Sivers <derek@sivers.org>
2015-11-10 18:48:07 +1300

    now API tests pass

--- now/schema.sql
+++ now/schema.sql
@@ -33,3 +33,51 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+----------------------------------------
+------------------------- API FUNCTIONS:
+----------------------------------------
+
+-- now.urls missing person_id
+CREATE OR REPLACE FUNCTION now.unknowns(
+	OUT status smallint, OUT js json) AS $$
+BEGIN
+	status := 200;
+	js := json_agg(r) FROM
+		(SELECT id, short, long FROM now.urls WHERE person_id IS NULL) r;
+	IF js IS NULL THEN js := '[]'; END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PARAMS: now.urls.id
+CREATE OR REPLACE FUNCTION now.url(integer,
+	OUT status smallint, OUT js json) AS $$
+BEGIN
+	status := 200;
+	js := row_to_json(r.*) FROM now.urls r WHERE id = $1;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PARAMS: now.urls.id
+CREATE OR REPLACE FUNCTION now.unknown_find(integer,
+	OUT status smallint, OUT js json) AS $$
+BEGIN
+	status := 200;
+	js := json_agg(r) FROM (SELECT * FROM peeps.people_view WHERE id IN
+		(SELECT * FROM now.find_person($1))) r;
+	IF js IS NULL THEN js := '[]'; END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- PARAMS: now.urls.id, person_id
+CREATE OR REPLACE FUNCTION now.unknown_assign(integer, integer,
+	OUT status smallint, OUT js json) AS $$
+BEGIN
+	status := 200;
+	UPDATE now.urls SET person_id = $2 WHERE id = $1;
+	js := row_to_json(r.*) FROM now.urls r WHERE id = $1;
+END;
+$$ LANGUAGE plpgsql;
+
--- now/test.rb
+++ now/test.rb
@@ -1,10 +1,64 @@
+P_SCHEMA = File.read('../peeps/schema.sql')
+P_FIXTURES = File.read('../peeps/fixtures.sql')
 require '../test_tools.rb'
 
 class TestNow < Minitest::Test
 	include JDB
 
-	def test_nil
-		assert true
+	def test_find_person
+		res = DB.exec("SELECT * FROM now.find_person(2)")
+		assert_equal '2', res[0]['find_person']
+		res = DB.exec("SELECT * FROM now.find_person(3)")
+		assert_equal '3', res[0]['find_person']
+		res = DB.exec("SELECT * FROM now.find_person(4)")
+		assert_equal 0, res.ntuples
+		res = DB.exec("SELECT * FROM now.find_person(99)")
+		assert_equal 0, res.ntuples
 	end
 
+	def test_find_person_needs_to_match_domain
+		DB.exec("INSERT INTO peeps.urls(person_id, url) VALUES (5, 'http://www.loompa.net')")
+		res = DB.exec("SELECT * FROM now.find_person(4)")
+		assert_equal 0, res.ntuples
+		DB.exec("INSERT INTO peeps.urls(person_id, url) VALUES (5, 'http://www.oompa.net')")
+		res = DB.exec("SELECT * FROM now.find_person(4)")
+		assert_equal '5', res[0]['find_person']
+	end
+
+	def test_unknowns
+		qry('now.unknowns()')
+		assert_equal(@j, [
+			{id: 3, short: 'salt.com/now', long: 'http://salt.com/now/'},
+			{id: 4, short: 'oompa.net/now.html', long: 'http://oompa.net/now.html'},
+			{id: 5, short: 'gongli.cn/now', long: nil}])
+	end
+
+	def test_url
+		qry('now.url(2)')
+		assert_equal(@j, {id: 2,
+			person_id: 2,
+			created_at: '2015-11-10',
+			updated_at: '2015-11-10',
+			tiny: 'wonka',
+			short: 'wonka.com/now',
+			long: 'http://www.wonka.com/now/'})
+	end
+
+	def test_unknown_find
+		qry('now.unknown_find(4)')
+		assert_equal([], @j)
+		qry('now.unknown_find(3)')
+		assert_equal([{id:3, name:'Veruca Salt', email:'veruca@salt.com', email_count:4}], @j)
+	end
+
+	def test_unknown_assign
+		qry('now.unknown_assign(3, 3)')
+		assert_equal(@j, {id: 3,
+			person_id: 3,
+			created_at: '2015-11-10',
+			updated_at: '2015-11-10',
+			tiny: 'salt',
+			short: 'salt.com/now',
+			long: 'http://salt.com/now/'})
+	end
 end
