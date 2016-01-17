0dcbc6118bb16db2d0864af0feb529b45d1d6871
Derek Sivers <derek@sivers.org>
2016-01-17 15:06:06 +0800

    lat.new_pairing() only needs 1, so LIMIT 1

--- lat/README.md
+++ lat/README.md
@@ -1,4 +1,5 @@
 # TODO:
 
 * web interface for untagged concepts
+* new_pairing() function: instead of order by random(), do offset by random method?  Only thing is, I don't know how to calculate number of rows without re-doing the entire join query.
 
--- lat/schema.sql
+++ lat/schema.sql
@@ -115,7 +115,7 @@ BEGIN
 		LEFT JOIN lat.pairings p ON (
 			(c1.id=p.concept1_id AND c2.id=p.concept2_id) OR
 			(c1.id=p.concept2_id AND c2.id=p.concept1_id)
-		) WHERE c1.id != c2.id AND p.id IS NULL ORDER BY RANDOM();
+		) WHERE c1.id != c2.id AND p.id IS NULL ORDER BY RANDOM() LIMIT 1;
 	IF id1 IS NULL THEN
 		RAISE EXCEPTION 'no unpaired concepts';
 	END IF;
