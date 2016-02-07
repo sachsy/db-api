30a5761246889495a7220830a478a53992ca6b67
Derek Sivers <derek@sivers.org>
2016-02-07 21:53:13 +1300

    formletters.accesskey set to NULL if empty

--- peeps/triggers.sql
+++ peeps/triggers.sql
@@ -246,3 +246,17 @@ CREATE TRIGGER clean_inkey
 	FOR EACH ROW EXECUTE PROCEDURE peeps.clean_inkey();
 
 
+-- If formletters.accesskey is '', change it to NULL before saving
+CREATE OR REPLACE FUNCTION peeps.null_accesskey() RETURNS TRIGGER AS $$
+BEGIN
+	IF btrim(NEW.accesskey) = '' THEN
+		NEW.accesskey = NULL;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+DROP TRIGGER IF EXISTS null_accesskey ON peeps.people CASCADE;
+CREATE TRIGGER null_accesskey
+	BEFORE INSERT OR UPDATE OF accesskey ON peeps.formletters
+	FOR EACH ROW EXECUTE PROCEDURE peeps.null_accesskey();
+
